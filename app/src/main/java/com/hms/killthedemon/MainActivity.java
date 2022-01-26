bf269ce2ebfce704e081ab58a9b000a49025fdb0
ChristianWiradinata <wiradinatac@gmail.com>
2022-01-26 16:39:27 +0700

    a

--- app/src/main/java/com/hms/killthedemon/MainActivity.java
+++ app/src/main/java/com/hms/killthedemon/MainActivity.java
@@ -12,10 +12,6 @@ import android.widget.Toast;
 import com.huawei.hmf.tasks.OnFailureListener;
 import com.huawei.hmf.tasks.OnSuccessListener;
 import com.huawei.hmf.tasks.Task;
-import com.huawei.hms.ads.AdListener;
-import com.huawei.hms.ads.AdParam;
-import com.huawei.hms.ads.HwAds;
-import com.huawei.hms.ads.banner.BannerView;
 import com.huawei.hms.common.ApiException;
 import com.huawei.hms.support.account.AccountAuthManager;
 import com.huawei.hms.support.account.request.AccountAuthParams;
@@ -32,57 +28,10 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_SIGN_IN = 1000;
     private static final String TAG = "Account";
 
-    private static final int REFRESH_TIME = 60;
-
-    private BannerView bannerView;
-
-    private BannerView defaultBannerView;
-
-    private FrameLayout adFrameLayout;
-
-    private AdListener adListener = new AdListener() {
-        @Override
-        public void onAdLoaded() {
-            showToast("Ad loaded.");
-        }
-
-        @Override
-        public void onAdFailed(int errorCode) {
-            showToast(String.format(Locale.ROOT, "Ad failed to load with error code %d.", errorCode));
-        }
-
-        @Override
-        public void onAdOpened() {
-            showToast(String.format("Ad opened "));
-        }
-
-        @Override
-        public void onAdClicked() {
-            showToast("Ad clicked");
-        }
-
-        @Override
-        public void onAdLeave() {
-            showToast("Ad Leave");
-        }
-
-        @Override
-        public void onAdClosed() {
-            showToast("Ad closed");
-        }
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        loadDefaultBannerAd();
-
-        adFrameLayout = findViewById(R.id.ad_frame);
-
-        HwAds.init(this);
 
         findViewById(R.id.HuaweiIdAuthButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,19 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 silentSignInByHwId();
             }
         });
-    }
-
-    private void loadDefaultBannerAd() {
-        defaultBannerView = findViewById(R.id.hw_banner_view);
-        defaultBannerView.setAdListener(adListener);
-        defaultBannerView.setBannerRefresh(REFRESH_TIME);
-
-        AdParam adParam = new AdParam.Builder().build();
-        defaultBannerView.loadAd(adParam);
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
     private void silentSignInByHwId() {
@@ -134,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void dealWithResultOfSignIn(AuthAccount authAccount) {
-//        Toast.makeText(this, "code: getDisplayName: -> "+authAccount.getDisplayName(), Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "code: getDisplayName: -> "+authAccount.getDisplayName(), Toast.LENGTH_LONG).show();
         String username = authAccount.getDisplayName();
         Intent gotoDuel = new Intent(this, duel.class);
         gotoDuel.putExtra("name", username);
